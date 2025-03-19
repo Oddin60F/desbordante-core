a66f113841f15cc8992c278eb62176d667b569c9
Oddin60F <vanyakozhukov2004@gmail.com>
2025-03-20 00:47:18 +0300

    Add component classes for HyFD inner workings

---
+++ src/core/algorithms/cfd/cfdfinder/util/inductor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "algorithms/fd/hyfd/model/fd_tree.h"
+#include "algorithms/fd/hyfd/model/non_fd_list.h"
+
+namespace algos::cfdfinder {
+
+using NonFDList = hyfd::NonFDList;
+using FDTree = algos::hyfd::fd_tree::FDTree;
+
+class Inductor {
+private:
+    std::shared_ptr<FDTree> tree_;
+    std::vector<RawFD> max_non_fds_;
+
+    void SpecializeTreeForNonFd(boost::dynamic_bitset<> const& lhs_bits, size_t rhs_id);
+
+public:
+    explicit Inductor(std::shared_ptr<FDTree> tree) noexcept : tree_(std::move(tree)) {}
+
+    void UpdateFdTree(NonFDList&& non_fds);
+    
+    std::vector<RawFD> GetMaxNonFDs() {
+        return std::move(max_non_fds_);
+    }
+};
+
+}  // namespace algos::hyfd
---
+++ src/core/algorithms/cfd/cfdfinder/util/sampler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "fd/hyfd/sampler.h"
+
+namespace algos::cfdfinder {
+
+// CFDFinder has exactly the same logic in Sampler as HyFD
+using hyfd::Sampler;
+
+}  // namespace algos::cfd::cfdfinder
